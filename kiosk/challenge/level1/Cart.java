ef302a5c670315be84646c16282d45786094309f
rang <withghd@gmail.com>
2025-03-11 20:57:24 +0900

    키오스크 도전 Lv 1

---
+++ kiosk/challenge/level1/Cart.java
@@ -0,0 +1,43 @@
+package challenge.level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private List<CartItem> cartItems;
+
+    Cart() {
+        this.cartItems = new ArrayList<>();
+    }
+
+    public void addCartItem(MenuItem menuItem) {
+        for (CartItem cartItem : cartItems) {
+            if (cartItem.getMenuItem().getName().equals(menuItem.getName())) {
+                cartItem.addQuantity();
+                return;
+            }
+        }
+        cartItems.add(new CartItem(menuItem));
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public int getTotalPrice() {
+        int total = 0;
+        for (CartItem cartItem : cartItems) {
+            total += cartItem.getTotalPrice();
+        }
+        return total;
+    }
+
+    public void clearCart() {
+        cartItems.clear();
+    }
+
+    public boolean isEmpty() {
+        return cartItems.isEmpty();
+    }
+}
---
+++ kiosk/challenge/level1/MenuItem.java
@@ -0,0 +1,26 @@
+package challenge.level1;
+
+public class MenuItem {
+
+    private String name;
+    private int price;
+    private String desc;
+
+    MenuItem(String name, int price, String desc) {
+        this.name = name;
+        this.price = price;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
